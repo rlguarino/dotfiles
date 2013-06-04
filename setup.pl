dc030c3180fa8b3eab5549d18b4257cc20d5e18c
Ross Guarino <rguarino@exablox.com>
2013-06-04 10:50:05 -0700

    Update installer and the ssh config

--- setup.pl
+++ setup.pl
@@ -9,8 +9,8 @@ my $type = $ARGV[1];
 system("git clone git://github.com/ehouse/dotfiles.git $homedir/dotfiles");
 system("curl http://j.mp/spf13-vim3 -L > spf13-vim.sh && sh spf13-vim.sh");
 system("curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh");
-system("ln -s ~/.dotfiles/tmux/tmux.conf ~/.tmux.conf");
-system("ln -s ~/.dotfiles/ssh/config ~/.ssh/config");
+system("ln -s ~/dotfiles/tmux/tmux.conf ~/.tmux.conf");
+system("ln -s ~/dotfiles/ssh/config ~/.ssh/config");
 system("chmod 644 ~/.ssh/config");
 
 print "Setup Complete\n";
