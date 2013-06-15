fa9ab74462248025eff20a7921878378d6efbedc
Ross Guarino <ross@siberia.csh.rit.edu>
2013-06-15 04:49:09 -0400

    Removed vim.nox alias

--- zsh/override.zsh
+++ zsh/override.zsh
@@ -3,7 +3,6 @@ ZSH_THEME="gentoo"
 unsetopt correct_all
 export PATH=/usr/local/bin:/usr/bin:/bin:/usr/local/games:/usr/games:/sbin/
 alias tmux="tmux -2"
-alias vim="vim.nox"
 export EDITOR = vim
 export VISUAL = vim
 export TERM=xterm-256color
