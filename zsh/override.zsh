2ce024dfb9005f26e27a512623f10d8104650024
Ross Guarino <rssguar@gmail.com>
2013-07-22 12:11:00 -0700

    Fixed vim commit editing, added virtual env support

--- zsh/override.zsh
+++ zsh/override.zsh
@@ -3,8 +3,14 @@ ZSH_THEME="gentoo"
 unsetopt correct_all
 export PATH=/usr/local/bin:/usr/bin:/bin:/usr/local/games:/usr/games:/sbin/
 alias tmux="tmux -2"
-export EDITOR = vim
-export VISUAL = vim
+
+export WORKON_HOME=$HOME/.virtualenvs
+export MSYS_HOME=/c/msys/1.0
+source /usr/local/bin/virtualenvwrapper.sh
+
+
+export EDITOR=vim
+export VISUAL=vim
 export TERM=xterm-256color
 
 #### END OF FILE
