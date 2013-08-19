018e5238082e2e38e22992e7bd16d52d9832b4f2
Ross Guarino <rlg8826@rit.edu>
2013-08-18 23:38:24 -0400

    Added setup.pl not takes an argument. Added fish
    setup.pl now takes a shell path an an artument
    Usage: setup.pl -s path/to/shell

--- README.md
+++ README.md
@@ -3,20 +3,26 @@
 ###Written By [Ross Guarino](https://github.com/rosslg)
 [![endorse](http://api.coderwall.com/rosslg/endorsecount.png)](http://coderwall.com/rosslg)
 
-
 My Personalized Environment Configuration Files
-
+I added fish to the configuration but I am leabing zsh for compatability with other systems.
+If you do not wish to use fish then you do not need to worry when that line in the install fails.
 
 ###Clone
 Clone this repo using the command: 
+'''
 git clone https://github.com/rguarino/dotfiles.git
+'''
 
 ###Setup
 Run the install located in the bin folder of the repo
+'''
+./dotfiles/setup.pl
+'''
 
 ###Setting up vim
 To install vim bundles start vim then run:
 :BuneleInstall
+For more information visit the [project page](http://github.com/gmarik/vundle)
 
 ###Dependencies
-zsh, tmux, vim
+fish, zsh, tmux, vim
--- setup.pl
+++ setup.pl
@@ -4,18 +4,35 @@ use warnings;
 
 my $user = $ENV{'LOGNAME'};
 my $homedir = $ENV{'HOME'};
-my $type = $ARGV[1];
+my $argc = @ARGV;
+my $type = $ARGV[0];
+my $arg1 = $ARGV[1];
 
-system("git clone git://github.com/ehouse/dotfiles.git $homedir/dotfiles");
+system("git clone https://github.com/rguarino/dotfiles.git $homedir/dotfiles");
 system("curl http://j.mp/spf13-vim3 -L > spf13-vim.sh && sh spf13-vim.sh");
 system("curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh");
 system("ln -s ~/dotfiles/tmux/tmux.conf ~/.tmux.conf");
 system("ln -s ~/dotfiles/ssh/config ~/.ssh/config");
 system("chmod 644 ~/.ssh/config");
-
+system("ln -s ~/dotfiles/fish/config.fish ~/.config/fish/config.fish");
 system("ln -s $homedir/dotfiles/vim/vimrc.local $homedir/.vimrc.local");
 system("ln -s $homedir/dotfiles/zsh/override.zsh $homedir/.oh-my-zsh/custom/override.zsh");
+system("sudo su -c \'echo \"/usr/bin/fish/\" >> /etc/shells\'");
+print "Type : $type\n";
+print "Type : $arg1\n";
 print "Setup Complete\n";
+if($argc >= 2){
+    if( $type eq "-s"){
+        system("chsh -s $arg1");
+    }
+}
 print "Please Restart Terminal for Changes to Take Effect\n";
+if($argc != 2){
+    print "run \'chsh -s /usr/bin/fish\' to change your shell to fish\n";
+    print "or \'chsh -s /bin/zsh\' to change your shell to zsh\n";
+}
+print "To remove the numbers plugin run BundleClean and then rm -rf ~/.vimview\n";
+print "View the github page https://github.com/fish-shell/fish-shell.git\n";
+print "Have Fun!";
 
 ### END OF FILE
