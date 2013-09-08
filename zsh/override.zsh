01ac62713c4eb7f6189254bc11642f088d968620
Ross Guarino <rlg8826@rit.edu>
2013-09-08 13:30:39 -0400

    Added go environment variables

--- zsh/override.zsh
+++ zsh/override.zsh
@@ -1,16 +1,19 @@
 # Customize to your needs...
 ZSH_THEME="gentoo"
 unsetopt correct_all
-export PATH=/usr/local/bin:/usr/bin:/bin:/usr/local/games:/usr/games:/sbin/
+
 alias tmux="tmux -2"
 
 export WORKON_HOME=$HOME/.virtualenvs
 export MSYS_HOME=/c/msys/1.0
 source /usr/local/bin/virtualenvwrapper.sh
 
+#Set go development variables
+export GOROOT=/usr/local/go
 
 export EDITOR=vim
 export VISUAL=vim
 export TERM=xterm-256color
 
+export PATH=/usr/local/bin:/usr/bin:/bin:/usr/local/games:/usr/games:/sbin/:/usr/local/go/bin:$PATH
 #### END OF FILE
