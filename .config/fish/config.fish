123c40313ca5ca270c055b03eaa7860df6127acd
Ross Guarino <rssguar@gmail.com>
2016-08-12 09:23:27 -0700

    Add gopath to fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,9 @@
 
 set -xg VISUAL vim
 set -xg EDITOR vim
+set -xg GOPATH $HOME
+
+set -xg PATH $PATH $GOPATH/bin
 
 # Ensure this happens after all other modifications to $PATH
 eval (python -m virtualfish)
